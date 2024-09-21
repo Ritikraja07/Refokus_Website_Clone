acb269cbe59c5f92f9ecff75aa6b89e5cd2419ca
Ritikraja07 <Ritik.rock121@gmail.com>
2024-09-21 20:38:01 +0530

    refokus website

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Navbar from './components/Navbar';
+import Work from './components/Work';
+import Stripes from './components/Stripes';
+import Products from './components/Products';
+import Marquees from './components/Marquees';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import LocomotiveScroll from 'locomotive-scroll';
+import './App.css'
+
+
+
+const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <div className="w-full h-full bg-zinc-900 text-white">
+      <Navbar />
+      <Work />
+      <Stripes />
+      <Products />
+      <Marquees />
+      <Cards />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { IoIosReturnRight } from "react-icons/io";
+
+function Button({title = "Get Started"}) {
+  return (
+    <div className='max-w-40 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between'>
+        <span className='text-sm font-medium'>{title}</span>
+        <IoIosReturnRight />
+    </div>
+  )
+}
+
+export default Button
